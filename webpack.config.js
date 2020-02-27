b8f1422999a1ab1916fa68af1945e5986210ee00
Gabe M <hey@gabe.mx>
2020-02-26 20:47:12 -0700

    chore: first commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+}
